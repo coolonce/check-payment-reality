6e9210489fecbc97bf934bcb6d2663cd0117ad1c
swpi <coolonce@yandex.ru>
2021-10-10 16:25:21 +0300

    fix

--- main.py
+++ main.py
@@ -21,48 +21,46 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 bot = Bot(token=TOKEN)
 
 bx24 = Bitrix24('https://property.bitrix24.ru/rest/2955/0uxepsze4xzwu3vk/')
+test_config=None
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 
-    
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
 
-    # a simple page that says hello
-    @app.route('/success', methods=["GET"])
-    def success():
-        tg_id = request.args.get("invId")
-        # result = bx24.callMethod('crm.deal.list', filter={'ID': tg_id})
-        result = bx24.callMethod('crm.deal.get/?id='+str(tg_id))
-        print(result)
-        send_message(result)
-        return "true"
-    
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    
+# a simple page that says hello
+@app.route('/success', methods=["GET"])
+def success():
+    tg_id = request.args.get("invId")
+    # result = bx24.callMethod('crm.deal.list', filter={'ID': tg_id})
+    result = bx24.callMethod('crm.deal.get/?id='+str(tg_id))
+    print(result)
+    send_message(result)
+    return "true"
 
-    @app.route('/')
-    def hello():
-        return "hello"
 
-    return app
+
+
+@app.route('/')
+def hello():
+    return "hello"
 
 def send_message(deal):
     chat_id = deal['TITLE'].split(':')[1]
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-from main import create_app
+from main import app
 if __name__ == "__main__":
-  app = create_app()
   app.run(host='0.0.0.0', port=80)
